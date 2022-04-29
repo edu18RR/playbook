8a189433ec5e0220725d207a3e6ba6e0523122d6
edu18RR <edu.rosas.16@gmail.com>
2022-04-29 09:31:28 -0500

    se agrego el segundo example1.2

---
+++ weekly_mission_2/example1/example1_2.js
@@ -0,0 +1,15 @@
+// Ejemplo 2: Crear un objeto con propiedades (es como un diccionario)
+const explorer = {
+    name: "Edu18RR",
+    age: 31,
+    mission: "Node JS",
+    stack:["js","node","react", "vue"],
+    blog: {
+        url: "https://github.com/edu18RR",
+        post: 5
+}
+  }
+  //console.log("Accediendo a las propiedades del objeto") tambien muestra la informacion al inicio depende de la posición
+  console.log(explorer)
+  console.log("Accediendo a las propiedades del objeto") // ingresará a donde se refiere
+  console.log(explorer.name)
