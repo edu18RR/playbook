8a934204f8d44881d0a34922bea792c7f0ca4897
edu18RR <edu.rosas.16@gmail.com>
2022-05-13 00:58:37 -0500

    modelado de Facebook

---
+++ weekly_mission_2/exercises/exercises1/Facebook.js
@@ -0,0 +1,37 @@
+const Facebook={
+    User:{
+        name:"Rangel Edu",
+        perfil_picture:"/imageperfil",
+        age:31,
+        friends:309,
+    },
+    Post:{
+        post_1:{
+            tilte:"edu y vero",
+            date:"07/05/2022",
+            content:{
+                text:"tu!",
+                image:"/image",
+            }
+        },
+        post_2:{
+            tilte:"second post",
+            date:"06/05/2022",
+            content:{
+                text:"Y el premio es para...",
+                image:"/image",
+                tilte:"edu y vero",
+                date:"07/05/2022",
+                friends_tags:["edu y vero"]
+            }}
+    },
+    Biography:{
+        country:"México",
+        city:"CDMX",
+        work:"CCSF",
+        emotional_situation:"Casado",
+        School:"UNID"
+        
+    }
+}
+console.log(Facebook)
