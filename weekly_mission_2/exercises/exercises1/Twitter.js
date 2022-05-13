d09a7dc57b62d22ebc1ba5189889bc9de6993991
edu18RR <edu.rosas.16@gmail.com>
2022-05-13 01:02:33 -0500

    ejercicio 3, clase de Twitter

---
+++ weekly_mission_2/exercises/exercises1/Twitter.js
@@ -0,0 +1,19 @@
+
+const twitter={
+    user:{
+        user:"Eduardo Rosas",
+        username:"@19_erosas",
+        age:31,
+        followers:164,
+        following:9,
+        total_post:342
+    },
+    trendingtopic:{
+        redid:"5",
+        twitter:{
+            Tendencias:["1.Kendrick", "Chivas", "Ponce", "MyBTSTracks", "DebahniEscobar", "MoraleAndTheBigSteppers"]
+        }
+    },
+    hahstag:["#MyBTSTracks","#DebahniEscobar","#MrMoraleAndTheBigSteppers"]
+}
+console.log(twitter.trendingtopic.twitter)
