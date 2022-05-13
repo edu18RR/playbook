c119c00f1160465b013618e51cbef33c6142feb4
edu18RR <edu.rosas.16@gmail.com>
2022-05-13 01:01:33 -0500

    ejercicio 3, clase de Instagram

---
+++ weekly_mission_2/exercises/exercises1/Instagram.js
@@ -0,0 +1,46 @@
+//// ¿Que veo en intagram?
+/*
+Perfil del usuario
+    nombre
+    usuario
+    seguidores
+    seguidos
+    numeros de post
+    bio
+fotos de usuario
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario
+mensajes
+    usuario destinatario
+    listas de mensajes
+videos del usuario
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario
+reel del usuario
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario
+fotos etiquetadas
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario que etiqueta
+    usuario
+*/
+
+const ig_user={
+    name:"Eduardo Rosas",
+    username:"edu_rosas.19",
+    followers:107,
+    following:215,
+    total_post:21
+}
