f6751d349118b413c4a4a06a78e7b7ca3a69faa2
edu18RR <edu.rosas.16@gmail.com>
2022-05-22 01:33:12 -0500

    Creación de index

--- weekly_mission_3/lives/my_first_js_proyect/index.js
+++ weekly_mission_3/lives/my_first_js_proyect/index.js
@@ -1,4 +1,4 @@
 //Archivo primcipal y estructuras
 const Ajolonauta = require('./app/Ajolonauta')
-const woopa = new Ajolonauta("Woopa")
+const woopa = new Ajolonauta("Woopa") // resultado esperado
 console.log(woopa.name)
